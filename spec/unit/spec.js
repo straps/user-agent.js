e596064ca1f0714e0f4b7b47c43b1fa12f67de8c
Francesco <straps@jesi.integrasoftware.it>
2011-04-06 17:49:14 +0200

    Added iPhone support
    Signed-off-by: Tj Holowaychuk <tj@vision-media.ca>

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -174,6 +174,12 @@ describe '.parse()'
         '4.0.4',
         'iPad'
         )
+      parse(
+        'Mozilla/5.0 (iPhone; U; CPU OS 3_1_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0 Mobile/7B367 Safari/531.21.10',
+        'safari',
+        '4.0',
+        'iPhone'
+        )
     end
   end
 end
